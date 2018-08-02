39c93d74d1b12d205a761e50c9f82ee5cc7f355e
BasLaravel <bazzkingma@hotmail.com>
2018-08-02 13:44:17 +0200

    first commit

---
+++ app/Http/Controllers/Auth/RegisterConfirmationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\user;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RegisterConfirmationController extends Controller
+{
+     public function index(){
+        try{
+            User::where('confirmation_token', request('token'))
+                ->firstOrFail()
+                ->update(['confirmed' => 1,
+                          'confirmation_token' => NULL
+                ]);
+                
+        }catch(\Exception $e){
+    
+            //session()->flash('message', 'token niet herkend');
+            return redirect('/');
+        }
+       
+    
+               // session()->flash('message', 'Uw account is geactiveerd');
+                return redirect('/');
+        }
+    
+}
