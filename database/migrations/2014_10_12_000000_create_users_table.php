c9996bff981209524e00fb91dff54d2b4e7efdec
BasLaravel <bazzkingma@hotmail.com>
2018-08-17 14:09:55 +0200

    def1 17-8

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->boolean('confirmed')->default(0);
-            $table->string('confirmation_token', 50)->nullable()->unique();
+            $table->string('confirmation_token', 50)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
